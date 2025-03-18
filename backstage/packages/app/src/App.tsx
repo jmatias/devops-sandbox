c695f42dc80d9647ada9964e7981c85c2f8860e3
Javier Matias-Cabrera <javier@javiermatias.com>
2025-03-18 11:50:03 -0400

    Postgres password env variable

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, Route } from 'react-router-dom';
 import { apiDocsPlugin, ApiExplorerPage } from '@backstage/plugin-api-docs';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
 import {
   CatalogEntityPage,
   CatalogIndexPage,
@@ -56,9 +57,20 @@ const app = createApp({
       catalogIndex: catalogPlugin.routes.catalogIndex,
     });
   },
-  components: {
-    SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
-  },
+    components: {
+        SignInPage: props => (
+            <SignInPage
+                {...props}
+                auto
+                provider={{
+                    id: 'github-auth-provider',
+                    title: 'GitHub',
+                    message: 'Sign in using GitHub',
+                    apiRef: githubAuthApiRef,
+                }}
+            />
+        ),
+    },
 });
 
 const routes = (
