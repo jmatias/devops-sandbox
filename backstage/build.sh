da9e37107e72ce2829d3fcfd095a235b4005b80e
Javier Matias-Cabrera <javier@javiermatias.com>
2025-03-18 07:09:14 -0400

    --wip-- [skip ci]

--- backstage/build.sh
+++ backstage/build.sh
@@ -2,6 +2,7 @@
 
 VERSION=$1
 
-docker build --platform linux/amd64 -f packages/backend/Dockerfile -t 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest -t 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" . &&
+docker build --platform linux/amd64 -f packages/backend/Dockerfile -t 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest \
+  -t 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" . &&
   docker push 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" &&
   docker push 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest
