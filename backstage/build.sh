895fa36c490d003961ab442bbf38c6292160234f
Javier Matias-Cabrera <javier@javiermatias.com>
2025-03-18 07:23:56 -0400

    Re-generate backstage app

--- backstage/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=$1
-
-docker build --platform linux/amd64 -f packages/backend/Dockerfile -t 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest \
-  -t 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" . &&
-  docker push 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" &&
-  docker push 590184073526.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest
