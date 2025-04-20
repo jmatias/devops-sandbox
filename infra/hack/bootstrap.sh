fb2b52f001fec9229479afcff646128276e4db8e
Javier Matias-Cabrera <javier@javiermatias.com>
2025-04-20 17:13:26 -0400

    Re-enable celery

--- infra/hack/bootstrap.sh
+++ infra/hack/bootstrap.sh
@@ -17,40 +17,49 @@ install_argocd() {
   kubectl wait --for=condition=Ready pod/"$(kubectl get pod -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[0].metadata.name}')" -n argocd
 }
 
+add_ecr_repo() {
+  local namespace
+  namespace="$1"
+
+  local ecr_password
+  ecr_password=$(aws ecr get-login-password --region us-east-1 --profile tw-abbvie)
+
+  kubectl create namespace "$namespace" || true
+
+  kubectl delete secret ecr-registry-secret -n "$namespace" || true
+
+  kubectl -n "$namespace" create secret docker-registry ecr-registry-secret \
+    --docker-server=386757133934.dkr.ecr.us-east-1.amazonaws.com \
+    --docker-username=AWS \
+    --docker-password="$ecr_password" || true
+}
+
 add_ecr_repos() {
   local ecr_password
   ecr_password=$(aws ecr get-login-password --region us-east-1 --profile tw-abbvie)
 
-  kubectl create namespace backstage || true
-  kubectl create namespace celery-sandbox || true
-  kubectl create namespace argocd || true
-  kubectl create namespace cert-manager || true
+  add_ecr_repo "backstage"
+  add_ecr_repo "celery-sandbox"
+  add_ecr_repo "argocd"
+  add_ecr_repo "cert-manager"
+  add_ecr_repo "sdlc-trace"
+  add_ecr_repo "external-secrets"
 
-  kubectl delete secret ecr-registry-secret -n celery-sandbox || true
-  kubectl delete secret ecr-registry-secret -n argocd || true
-  kubectl delete secret ecr-registry-secret -n backstage || true
-  kubectl delete secret ecr-registry-secret -n cert-manager || true
+}
 
-  kubectl -n celery-sandbox create secret docker-registry ecr-registry-secret \
-    --docker-server=386757133934.dkr.ecr.us-east-1.amazonaws.com \
-    --docker-username=AWS \
-    --docker-password="$ecr_password" || true
-  kubectl -n argocd create secret docker-registry ecr-registry-secret \
-    --docker-server=386757133934.dkr.ecr.us-east-1.amazonaws.com \
-    --docker-username=AWS \
-    --docker-password="$ecr_password" || true
-  kubectl -n backstage create secret docker-registry ecr-registry-secret \
-    --docker-server=386757133934.dkr.ecr.us-east-1.amazonaws.com \
-    --docker-username=AWS \
-    --docker-password="$ecr_password" || true
-  kubectl -n cert-manager create secret docker-registry ecr-registry-secret \
-    --docker-server=386757133934.dkr.ecr.us-east-1.amazonaws.com \
-    --docker-username=AWS \
-    --docker-password="$ecr_password" || true
-  kubectl -n sdlc-trace create secret docker-registry ecr-registry-secret \
-    --docker-server=386757133934.dkr.ecr.us-east-1.amazonaws.com \
-    --docker-username=AWS \
-    --docker-password="$ecr_password" || true
+create_github_oauth_secrets() {
+
+  local namespace
+  namespace="$1"
+
+  kubectl create namespace "$namespace" || true
+
+  kubectl -n "$namespace" create secret generic github-auth \
+    --from-literal=AUTH_GITHUB_CLIENT_ID="$AUTH_GITHUB_CLIENT_ID" \
+    --from-literal=AUTH_GITHUB_CLIENT_SECRET="$AUTH_GITHUB_CLIENT_SECRET" \
+    --from-literal=AUTH_GITHUB_TEST_CLIENT_ID="$AUTH_GITHUB_TEST_CLIENT_ID" \
+    --from-literal=AUTH_GITHUB_TEST_CLIENT_SECRET="$AUTH_GITHUB_TEST_CLIENT_SECRET" \
+    --from-literal=GITHUB_TOKEN="$GITHUB_TOKEN" || true
 }
 
 install_apps() {
@@ -78,12 +87,10 @@ install_apps() {
     --from-literal=AUTH_GITHUB_TEST_CLIENT_ID="$AUTH_GITHUB_TEST_CLIENT_ID" \
     --from-literal=AUTH_GITHUB_TEST_CLIENT_SECRET="$AUTH_GITHUB_TEST_CLIENT_SECRET" || true
 
-  kubectl -n backstage create secret generic github-auth \
-    --from-literal=AUTH_GITHUB_CLIENT_ID="$AUTH_GITHUB_CLIENT_ID" \
-    --from-literal=AUTH_GITHUB_CLIENT_SECRET="$AUTH_GITHUB_CLIENT_SECRET" \
-    --from-literal=AUTH_GITHUB_TEST_CLIENT_ID="$AUTH_GITHUB_TEST_CLIENT_ID" \
-    --from-literal=AUTH_GITHUB_TEST_CLIENT_SECRET="$AUTH_GITHUB_TEST_CLIENT_SECRET" \
-    --from-literal=GITHUB_TOKEN="$GITHUB_TOKEN" || true
+  create_github_oauth_secrets "backstage"
+  create_github_oauth_secrets "celery-sandbox"
+  create_github_oauth_secrets "argocd"
+
 }
 
 set -e
