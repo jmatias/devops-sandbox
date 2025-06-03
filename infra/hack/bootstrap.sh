a7f7f1e6bbe14a5015c99f09e1de53235c736aa5
Javier Matias-Cabrera <javier@javiermatias.com>
2025-06-02 20:02:19 -0400

    Install prometheus CRDs manually
    Enable beats
    
    Enable server-side apply
    
    disable beats
    
    Disable serverside apply
    
    Set TTL to 60
    
    Downgrade to 1.32
    
    Remove fargate profiles
    
    Server side apply
    
    Install CRDs
    
    Update updater
    
    Writeback git
    
    Update eck stack
    
    Fix image-name
    
    Add more resources
    
    Enable logstash
    
    Use helm directly
    
    Bumo argocd to v2.14.12
    
    Update cluster config
    
    Update schema

--- infra/hack/bootstrap.sh
+++ infra/hack/bootstrap.sh
@@ -7,6 +7,7 @@ install_argocd() {
 
   kubectl apply -f "https://raw.githubusercontent.com/external-secrets/external-secrets/v0.16.1/deploy/crds/bundle.yaml"
 
+
   kubectl apply -n argocd -k ../apps/argocd/overlays/aws
 
   sleep 1
@@ -20,6 +21,18 @@ install_argocd() {
 }
 
 install_apps() {
+
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_prometheusagents.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_scrapeconfigs.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+  kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.82.0/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
+
   argocd login --core
   argocd app create app-of-apps \
     --upsert \
