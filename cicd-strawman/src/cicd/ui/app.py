186c773c203585edfec027ba396204c424bd78e0
Javier L. Matías-Cabrera <javier@javiermatias.com>
2025-06-04 10:08:49 -0400

    Hola mundo

--- cicd-strawman/src/cicd/ui/app.py
+++ cicd-strawman/src/cicd/ui/app.py
@@ -3,7 +3,7 @@ import gradio as gr
 def hello(name):
     return f"Hola, {name}!"
 
-app = gr.Interface(fn=hello, inputs="text", outputs="text", title="Hello World Gradio App")
+app = gr.Interface(fn=hello, inputs="text", outputs="text", title="¡Hola mundo!")
 
 if __name__ == "__main__":
     app.launch(server_name="0.0.0.0", server_port=7860, share=False)
