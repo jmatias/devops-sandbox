11911048589d8727f0790cac450cfd22ac8d3357
Javier L. Matías-Cabrera <javier@javiermatias.com>
2025-06-04 11:49:18 -0400

    Update app.py

--- cicd-strawman/src/cicd/ui/app.py
+++ cicd-strawman/src/cicd/ui/app.py
@@ -3,7 +3,7 @@ import gradio as gr
 def hello(name):
     return f"Hola, {name}!"
 
-app = gr.Interface(fn=hello, inputs="text", outputs="text", title="¡Hola mundo!")
+app = gr.Interface(fn=hello, inputs="text", outputs="text", title="Hello world!")
 
 if __name__ == "__main__":
     app.launch(server_name="0.0.0.0", server_port=7860, share=False)
