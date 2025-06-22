82b91aeb52cb7d59597d92153272e42330e85919
Javier Matias-Cabrera <javier@javiermatias.com>
2025-06-22 01:29:52 -0400

    re-build backstage

--- backstage/build.sh
+++ backstage/build.sh
@@ -2,9 +2,10 @@
 
 VERSION=$1
 
+set -e
 set -x
-#yarn tsc
-#yarn build:all
+yarn tsc
+yarn build:all
 
 docker build --platform linux/amd64 -t 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest -t 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" . &&
   docker push 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" &&
