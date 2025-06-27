507cb4caaf13683cbe2daf636e7b969f4f3d6eef
Javier Matias-Cabrera <javier@javiermatias.com>
2025-06-27 00:23:11 -0400

    Ingress class

--- infra/hack/bootstrap.sh
+++ infra/hack/bootstrap.sh
@@ -49,5 +49,5 @@ set -e
 install_argocd
 install_apps
 
-pushd $HOME/work/external-repos/autoscaler/vertical-pod-autoscaler/hack
-./vpa-up.sh
+#pushd $HOME/work/external-repos/autoscaler/vertical-pod-autoscaler/hack
+#./vpa-up.sh
--- src/backstage/build.sh
+++ src/backstage/build.sh
@@ -7,6 +7,4 @@ set -x
 yarn tsc
 yarn build:all
 
-docker build --platform linux/amd64 -t 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest -t 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" . &&
-  docker push 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" &&
-  docker push 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest
+docker buildx build --platform linux/amd64 --push -t 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:latest -t 386757133934.dkr.ecr.us-east-1.amazonaws.com/javier/backstage:"$VERSION" .
