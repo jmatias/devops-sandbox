abf4111fcc7b7769442f454130ca6e242433ffc8
Javier Matias-Cabrera <javier@javiermatias.com>
2025-08-25 20:55:44 -0400

    Enable vpa

--- infra/hack/bootstrap.sh
+++ infra/hack/bootstrap.sh
@@ -55,6 +55,6 @@ popd
 install_argocd
 install_apps
 
-#pushd $HOME/work/external-repos/autoscaler/vertical-pod-autoscaler/hack
-#./vpa-up.sh
-#pod || true
+pushd $HOME/work/external-repos/autoscaler/vertical-pod-autoscaler/hack
+FEATURE_GATES="InPlaceOrRecreate=true" ./vpa-up.sh
+popd || true
